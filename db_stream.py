ea09937cbf2b153ee5bec0246ae98d1343d0ff01
tadowdy <tadowdy@siue.edu>
2019-03-18 01:10:51 -0500

    finish on_data function

--- db_stream.py
+++ db_stream.py
@@ -2,28 +2,50 @@ import tweepy
 import mysql.connector
 from mysql.connector import Error
 import auth
+import json
+from dateutil import parser
+
+
+def connect(username, created_at, tweet, place, location):
+    """
+    connect to MySQL database and insert twitter data
+    """
+    try:
+        connect = mysql.connector.connect(
+            host=auth.HOST, database=auth.DATABASE,
+            user=auth.USER, password=auth.PASSWORD)
+
+        if connect.is_connected():
+            """
+            handle data
+            """
+            cursor = connect.cursor()
+            query = "INSERT INTO Golf (username, created_at, tweet, place, location) VALUES (%s, %s, %s, %s, %s)"
+            cursor.execute(
+                query, (username, created_at, tweet, location, place))
+    except Error as e:
+        print(e)
+    cursor.close()
+    connect.close()
 
 
 class TheStreamListener(tweepy.StreamListener):
     def on_status(self, status):
         print(status.text)
 
-    def connect(username, created_at, tweet, place, location):
-        """
-        connect to MySQL database and insert twitter data
-        """
+    def data(self, data):
         try:
-            connect = mysql.connector.connect(
-                host=auth.HOST, database=auth.DATABASE,
-                user=auth.USER, password=auth.PASSWORD)
-
-            if connect.is_connected():
-                """
-                handle data
-                """
-                cursor = connect.cursor()
-                query = "INSERT INTO Golf (username, created_at, tweet, place, location) VALUES (%s, %s, %s, %s, %s)"
-                cursor.execute(
-                    query, (username, created_at, tweet, location, place))
-        except Error as e:
-            print(e)
+            raw_data = json.loads(data)
+            if 'text' in raw_data:  # if there's text in the data
+                username = raw_data['user']['screen_name']
+                created_at = parser.parse(raw_data['created_at'])
+                tweet = raw_data['text']
+                location = raw_data['user']['location']
+                # if there's a place
+                if raw_data['place'] is not None:
+                    place = raw_data['place']['country']
+                else:
+                    place = None
+                connect(username, created_at, tweet, place, location)
+        except Error as error:
+            print(error)
