ac80d7b106f22322da284bb1e2fbc2e124a285b5
tadowdy <tadowdy@siue.edu>
2019-04-15 12:17:57 -0500

    set up pandas dataframe

--- tweet_analysis.py
+++ tweet_analysis.py
@@ -4,6 +4,7 @@ import mysql.connector
 import auth
 
 # connect to db
+connect = ""
 try:
     connect = mysql.connector.connect(
         host=auth.HOST, database=auth.DATABASE,
@@ -11,4 +12,8 @@ try:
 
 except Error as e:
     print(e)
+
+mysql_df = pd.read_sql("select * from tweets", con=connect)
+print("loaded dataframe. Records: ", len(mysql_df))
+
 connect.close()
