827c7f0b65b53bb5961412080a09bcc122f89ea7
tadowdy <tadowdy@siue.edu>
2019-04-29 11:16:48 -0500

    add print statements for logging. switch to sqlalchemy.

--- tweet_analysis.py
+++ tweet_analysis.py
@@ -1,38 +1,46 @@
 import textblob
 from textblob import sentiments
 import pandas as pd
-import mysql.connector
+import mysql.connector.dbapi
+from mysql.connector import Error
 import auth
+from sqlalchemy import create_engine
 
 # connect to db
 connect = ""
 try:
-    connect = mysql.connector.connect(
-        host=auth.HOST, database=auth.DATABASE,
-        user=auth.USER, password=auth.PASSWORD)
+    print("connecting...")
+    connect = create_engine(
+        "mysql+pymysql://root:password@localhost/twitter_db")
+    print("connected")
+    # connect = mysql.connector.connect(
+    #     host=auth.HOST, database=auth.DATABASE,
+    #     user=auth.USER, password=auth.PASSWORD)
 
 except Error as e:
     print(e)
 
 # create dataframes for both tables
+print("reading dataframe...")
 df = pd.read_sql("select * from tweets", con=connect)
-sent_df = pd.read_sql("select * from sentiments", con=connect)
-
+print("dataframe complete")
 # analyze tables and store data
+print("collecting scores...")
 for tweet in df['content']:
     analysis = textblob.TextBlob(tweet)
     analysis = analysis.sentiment
-    sent_df['sentiment_score'] = analysis[0]
-    sent_df['subjective_score'] = analysis[1]
+    df['sentiment_score'] = analysis[0]
+    df['subjective_score'] = analysis[1]
 
     # set approximate value
     if analysis[0] > 0:
-        sent_df['sentiment'] = "positive"
+        df['sentiment'] = "positive"
     elif analysis[0] < 0:
-        sent_df['sentiment'] = "negative"
+        df['sentiment'] = "negative"
     else:
-        sent_df['sentiment'] = "neutral"
-
-sent_df.to_sql("sentiments")
+        df['sentiment'] = "neutral"
+print("scores collected")
+print(df)
+df.to_sql("sentiments", connect, if_exists='append')
 
 connect.close()
