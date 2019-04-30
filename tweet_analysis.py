0633ec92870addf5bf907e01866529c8b6a9fcf6
tadowdy <tadowdy@siue.edu>
2019-04-30 11:56:34 -0500

    remove sql connector code. use tweets table.

--- tweet_analysis.py
+++ tweet_analysis.py
@@ -1,8 +1,13 @@
+"""
+last updated: 04/30/2019
+
+Notes:
+pandas requires using sqlalchemy--NOT mysql connector
+"""
+
 import textblob
 from textblob import sentiments
 import pandas as pd
-import mysql.connector.dbapi
-from mysql.connector import Error
 import auth
 from sqlalchemy import create_engine
 
@@ -13,17 +18,16 @@ try:
     connect = create_engine(
         "mysql+pymysql://root:password@localhost/twitter_db")
     print("connected")
-    # connect = mysql.connector.connect(
-    #     host=auth.HOST, database=auth.DATABASE,
-    #     user=auth.USER, password=auth.PASSWORD)
 
-except Error as e:
-    print(e)
+except:
+    print("couldn't connect to the database")
+    exit()
 
 # create dataframes for both tables
 print("reading dataframe...")
 df = pd.read_sql("select * from tweets", con=connect)
 print("dataframe complete")
+
 # analyze tables and store data
 print("collecting scores...")
 for tweet in df['content']:
@@ -41,6 +45,6 @@ for tweet in df['content']:
         df['sentiment'] = "neutral"
 print("scores collected")
 print(df)
-df.to_sql("sentiments", connect, if_exists='append')
+df.to_sql("tweets", connect, if_exists='append')
 
 connect.close()
